0f10f30ec9fc81cbdc425d6df428a8fc8eee4305
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-23 13:00:41 +0500

    Add amount toggling functionality, add useEffect to update amount and total of the cartSlice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -17,9 +17,27 @@ const cartSlice = createSlice({
     },
     removeItem: (state, action) => {
       state.cartItems = state.cartItems.filter((item) => item.id !== action.payload)
+    }, 
+    increase: (state, {payload}) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount += 1;
     },
-  }
+    decrease: (state, {payload}) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount -= 1;
+    },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      })
+      state.amount = amount;
+      state.total = total;
+    },
+  },
 });
 
-export const { clearCart, removeItem } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease, calculateTotals } = cartSlice.actions;
 export default cartSlice.reducer;
