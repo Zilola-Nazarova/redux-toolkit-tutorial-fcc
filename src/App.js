9e75602951eb700e157886b196d3c2710dc48ba2
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-08-23 22:55:04 +0500

    Replace the hardcoded data with async function that fetches data from API and returns a promise

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import Navbar from './components/Navbar';
 import CartContainer from './components/CartContainer';
 import Modal from './components/Modal';
 import { useSelector, useDispatch } from 'react-redux';
-import { calculateTotals } from './features/cart/cartSlice';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
 import { useEffect } from 'react';
 
 function App() {
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(calculateTotals());
-  }, [cartItems])
+  }, [cartItems]);
 
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+  
+  if (isLoading) {
+    return <div className='loading'>
+      <h1>Loading...</h1>
+    </div>
+  }
   return (
   <main>
     {isOpen && <Modal />}
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,12 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from '../../cartItems';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const url = 'https://course-api.com/react-useReducer-cart-project';
 
 const initialState = {
-  cartItems: cartItems,
+  cartItems: [],
   amount: 3,
   total: 0,
   isLoading: true,
 };
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+  return fetch(url)
+  .then(resp => resp.json())
+  .catch((err) => console.log(err));
+});
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -17,7 +24,7 @@ const cartSlice = createSlice({
     },
     removeItem: (state, action) => {
       state.cartItems = state.cartItems.filter((item) => item.id !== action.payload)
-    }, 
+    },
     increase: (state, {payload}) => {
       const cartItem = state.cartItems.find((item) => item.id === payload.id);
       cartItem.amount += 1;
@@ -37,7 +44,20 @@ const cartSlice = createSlice({
       state.total = total;
     },
   },
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.rejected]: (state) => {
+      state.isLoading = false;
+    },
+  },
 });
 
 export const { clearCart, removeItem, increase, decrease, calculateTotals } = cartSlice.actions;
+
 export default cartSlice.reducer;
